7dfdb9a4cf72fc685b82c80f940bbfbe1c4ddb9b
tomyleca <tomyleca14@gmail.com>
2024-07-04 00:04:01 -0300

    terminado

--- programa.pl
+++ programa.pl
@@ -1,1 +1,48 @@
-% Aquí va el código.
+persona(nora,empleado,compras).
+persona(pedro,cadete,compras).
+persona(roque,pasante,ventas).
+persona(maria,empleado,ventas).
+persona(juan,cadete,ventas).
+persona(felipe,empleado,administracion).
+persona(hugo,empleado,administracion).
+
+trabajaEnVentas(Persona):-persona(Persona,_,ventas).
+trabajaEnCompras(Persona):-persona(Persona,_,compras).
+
+mismoDepartamento(Persona1,Persona2):- persona(Persona1,_,Departamento), persona(Persona2,_,Departamento).
+
+cargoSuperior(Persona1,Persona2):-
+    persona(Persona1,empleado,_),
+    not(persona(Persona2,empleado,_)),
+     mismoDepartamento(Persona1,Persona2).
+
+/* 
+1
+?- trabajaEnVentas(felipe).
+false.
+
+?- trabajaEnVentas(roque). 
+true.
+
+2
+?- mismoDepartamento(roque,juan).    
+true.
+
+?- mismoDepartamento(pedro,juan). 
+false.
+
+3
+
+
+?- cargoSuperior(roque,maria).
+false.
+
+?- cargoSuperior(roque,juan).  
+false.
+
+?- cargoSuperior(maria,roque). 
+true.
+
+?- cargoSuperior(nora,juan).   
+false.
+\*
